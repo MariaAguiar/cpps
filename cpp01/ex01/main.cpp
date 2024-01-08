de410ff5a42d0d42be25889c9b891e688749c6bf
Margarida Aguiar <aguiar.m.margarida@gmail.com>
2024-01-08 10:03:34 +0000

    ex01 v2 and ex02

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -1,25 +1,30 @@
-
+#include <iostream>
 #include <sstream>
 #include "Zombie.h"
 
 int main( int ac, char **av )
 {
-	if (ac > 1)
+	if (ac == 2)
 	{
 		int	i;
 		std::stringstream ss(av[1]);
 		ss >> i;
 		if (ss.fail())
+		{
+			std::cout << "Invalid Argument" << std::endl;
 			return (2);
+		}
 		Zombie* horde = zombieHorde( i, "Heap Soldier" );
 		if (!horde)
 			return (2);
 		for (int t = 0; t < i; t++)
 		{
-			std::cout << t << ": ";
+			std::cout << t + 1 << ": ";
 			horde[t].announce();
 		}
 		delete[] horde;
 	}
+	else
+		std::cout << "Invalid Number of Arguments" << std::endl;
 	return 1;
 }
