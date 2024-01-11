30c195f114047942d5115a464bec3487f6982ebd
Margarida <aguiar.m.margarida@gmail.com>
2024-01-11 08:40:43 +0000

    corrected cpp01, ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -15,6 +15,11 @@ int main( int ac, char **av )
 	std::ifstream filename;
   	filename.open (av[1], std::ifstream::binary);
 	
+	if (filename.fail())
+	{
+		std::cerr << "Couldn't open file" << std::endl;
+		return 1;
+	}
 	std::string line;
 	std::string outfile = av[1];
 	outfile += ".replace";
@@ -24,27 +29,34 @@ int main( int ac, char **av )
 	int sum = 0;
 	char c;
     output.flush();
+	std::string s2 = av[2];
 	while (std::getline(filename, s))
 	{
 		size_t pos = 0;
+		sum += s.size();
 		while (pos < s.size() - 1)
 		{
-			std::string s2 = av[2];
 			pos = s.find(s2, pos);
-			if (pos >= s.size() - 1)
-				break;
+			if (pos == std::string::npos)
+				break ;
 			int i = 0;
 			for (i = 0; av[2][i]; i++)
 			{
 				if (av[3][i])
 					s[pos++] = av[3][i];
+				else
+				{
+					s.erase(pos, s2.size() - i);
+					break ;
+				}
 			}
-			while (av[3][i] && s[pos] != '\n' && s[pos] != '\0')
-				s[pos++] = av[3][i++];
 			if (av[3][i])
+			{
 				s.insert(pos, &av[3][i]);
+				std::string aux = &av[3][i];
+				pos += aux.size();
+			}
 		}
-		sum += s.size();
 		filename.seekg(sum, filename.beg);
 		filename.get(c);
 		sum++;
