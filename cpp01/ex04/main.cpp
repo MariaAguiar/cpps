68d8ed806d7563df13aade88a6fdc99aceb3c9d0
Maria margarida Nascimento aguiar <mnascime@c2r7s7.42lisboa.com>
2024-01-24 15:42:17 +0000

    less convoluted code

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 int main( int ac, char **av )
 {
@@ -17,41 +18,33 @@ int main( int ac, char **av )
 	}
 	std::ifstream filename;
   	filename.open (av[1], std::ifstream::binary);
-	
 	if (filename.fail())
 	{
 		std::cerr << "Couldn't open file" << std::endl;
 		return 1;
 	}
+
 	std::string line;
 	std::string outfile = av[1];
 	outfile += ".replace";
 	std::ofstream output(outfile.c_str());
+    output.flush();
 
 	std::string s;
-    output.flush();
-	int flag = 0;
-	size_t pos;
+	size_t pos = 0;
 	std::string s3 = av[3];
-	while (std::getline(filename, s))
+	std::stringstream	ss;
+	
+	ss << filename.rdbuf();
+	s = ss.str();
+	pos = s.find(s2, 0);
+	while (pos != std::string::npos)
 	{
-		pos = 0;
-		while (s2.size() && pos < s.size())
-		{
-			pos = s.find(s2, pos);
-			if (pos == std::string::npos)
-				break ;
-			s.erase(pos, s2.size());
-			s.insert(pos, av[3]);
-			pos += s3.size();
-		}
-		if (flag == 1)
-			output << '\n';
-		output << s;
-		flag = 1;
+		s.erase(pos, s2.size());
+		s.insert(pos, s3);
+		pos = s.find(s2, pos + s3.size());
 	}
-	if (s == "\0" && flag == 1)
-		output << "\n";
+	output << s;
 	output.close();
   	filename.close();
 
