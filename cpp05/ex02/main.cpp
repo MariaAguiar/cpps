ec19c521be0f2cbabdc743c296e65b4137d074e0
Maria margarida Nascimento aguiar <mnascime@c2r4s6.42lisboa.com>
2024-03-19 19:15:24 +0000

    redid main

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,22 +6,125 @@
 
 int main( void )
 {
-	try {
-		Bureaucrat j( "Zee best", 5 );
-		std::cout << j;
-		j.upGrade();
-		std::cout << j;
-		RobotomyRequestForm    f = RobotomyRequestForm( "" );
-		PresidentialPardonForm p( "" );
-		ShrubberyCreationForm  s( "" );
-		f.beSigned( j );
-		p.beSigned( j );
-		s.beSigned( j );
-		j.executeForm( f );
-		j.executeForm( p );
-		j.executeForm( s );
-	} catch ( const std::exception &e ) {
-		std::cout << e.what() << std::endl;
+	{
+		std::cout << "Scenario 1 (FAIL)" << std::endl;
+		try
+		{
+			Bureaucrat b1("Pretty bad Bureaucrat", 150);
+			ShrubberyCreationForm f1("An Unimportant Someone");
+			// b1.downGrade();
+			b1.signForm(f1);
+			// f1.beSigned(b1);
+			// b1.signForm(f1);
+			f1.execute(b1);
+		}
+		// catch (const Bureaucrat::GradeTooHighException& e)
+		catch (const std::exception &e)
+		{
+			std::cerr << "Error: " << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	{
+		std::cout << "Scenario 2 (SUCCESS)" << std::endl;
+		try
+		{
+			Bureaucrat b1("Stellar Bureaucrat", 2);
+			ShrubberyCreationForm f1("Someone Important");
+			// b1.upGrade();
+			b1.signForm(f1);
+			// f1.beSigned(b1);
+			// b1.signForm(f1);
+			f1.execute(b1);
+			f1.execute(b1);
+		}
+		// catch (const Bureaucrat::GradeTooHighException& e)
+		catch (const std::exception &e)
+		{
+			std::cerr << "Error: " << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	{
+		{
+		std::cout << "Scenario 3 (FAIL)" << std::endl;
+		try
+		{
+			Bureaucrat b1("Pretty bad Bureaucrat", 150);
+			RobotomyRequestForm f1("An Unimportant Someone");
+			// b1.downGrade();
+			b1.signForm(f1);
+			// f1.beSigned(b1);
+			// b1.signForm(f1);
+			f1.execute(b1);
+			f1.execute(b1);
+		}
+		// catch (const Bureaucrat::GradeTooHighException& e)
+		catch (const std::exception &e)
+		{
+			std::cerr << "Error: " << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	{
+		std::cout << "Scenario 4 (SUCCESS)" << std::endl;
+		try
+		{
+			Bureaucrat b1("Stellar Bureaucrat", 2);
+			RobotomyRequestForm f1("Someone Important");
+			// b1.upGrade();
+			b1.signForm(f1);
+			// f1.beSigned(b1);
+			// b1.signForm(f1);
+			f1.execute(b1);
+			f1.execute(b1);
+		}
+		// catch (const Bureaucrat::GradeTooHighException& e)
+		catch (const std::exception &e)
+		{
+			std::cerr << "Error: " << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	}
+		{
+		std::cout << "Scenario 5 (FAIL)" << std::endl;
+		try
+		{
+			Bureaucrat b1("Pretty bad Bureaucrat", 150);
+			PresidentialPardonForm f1("An Unimportant Someone");
+			// b1.downGrade();
+			b1.signForm(f1);
+			// f1.beSigned(b1);
+			// b1.signForm(f1);
+			f1.execute(b1);
+			f1.execute(b1);
+		}
+		// catch (const Bureaucrat::GradeTooHighException& e)
+		catch (const std::exception &e)
+		{
+			std::cerr << "Error: " << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	{
+		std::cout << "Scenario 6 (SUCCESS)" << std::endl;
+		try
+		{
+			Bureaucrat b1("Stellar Bureaucrat", 2);
+			PresidentialPardonForm f1("Someone Important");
+			// b1.upGrade();
+			b1.signForm(f1);
+			// f1.beSigned(b1);
+			// b1.signForm(f1);
+			f1.execute(b1);
+			f1.execute(b1);
+		}
+		// catch (const Bureaucrat::GradeTooHighException& e)
+		catch (const std::exception &e)
+		{
+			std::cerr << "Error: " << e.what() << std::endl;
+		}
 	}
 	return 0;
 }
